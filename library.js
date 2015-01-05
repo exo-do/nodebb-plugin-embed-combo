f9af30fcdcd12cd269984afe8a033e166663299d
h3ku <kuestatv@gmail.com>
2015-01-05 18:40:06 +0100

    Vine

--- library.js
+++ library.js
@@ -3,21 +3,27 @@
 
 	var ComboEmbed = {},
 		embedYoutube = '<div class="js-lazyYT" data-youtube-id="$1" data-width="640" data-height="360"><iframe class="lazytube" src="//www.youtube.com/embed/$1"></iframe></div>',
+       
         embedTwitter = '<span data-url="https://twitter.com/$2/statuses/$3"></span><script type="text/javascript" src="//api.twitter.com/1/statuses/oembed.json?id=$3&callback=twitterEmbed"></script>';
 
+        embedVine = '<iframe class="vine-embed" src="https://vine.co/v/$1/embed/postcard?related=0" width="480" height="480" frameborder="0"></iframe><script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>';
 
 	    var	regularUrlYoutube = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtube\.com)\/(?:watch\?v=)(.+)">.+<\/a>/g;
         var	shortUrlYoutube = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtu\.be)\/(.+)">.+<\/a>/g;
         var	embedUrlYoutube = /<a href="(?:https?:\/\/)?(?:www\.)youtube.com\/embed\/([\w\-_]+)">.+<\/a>/;
 
+
         var regularUrlTwitter = /<a href="(http|https):\/\/twitter.com\/([^\/"\s]*)\/status\/([^\/"\s]*)(\/photo\/1|)">.+?<\/a>/g
 
+        var regularUrlVine = /<a href="(?:https?:\/\/)?(?:vine\.co)\/v\/?(.+)">.+<\/a>/g;
 
 
     ComboEmbed.parse = function(data, callback) {
         if (!data || !data.postData || !data.postData.content) {
             return callback(null, data);
         }
+
+        //Youtube
         if (data.postData.content.match(embedUrlYoutube)) {
             data.postData.content = data.postData.content.replace(embedUrlYoutube, embedYoutube);
         }
@@ -27,9 +33,17 @@
         if (data.postData.content.match(shortUrlYoutube)) {
             data.postData.content = data.postData.content.replace(shortUrlYoutube, embedYoutube);
         }
+
+        //Twitter
         if (data.postData.content.match(regularUrlTwitter)) {
             data.postData.content = data.postData.content.replace(regularUrlTwitter, embedTwitter);
         }
+
+        //Vine
+        if (data.postData.content.match(regularUrlVine)) {
+            data.postData.content = data.postData.content.replace(regularUrlVine, embedVine);
+        }
+
         callback(null, data);
 
     };
